17aecc2458acdccc6639498e5a14c33550f2188a
herlanderbento <herlanderbento19@gmail.com>
2021-12-18 21:08:13 +0100

    first commit

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+  Icon,
+} from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/80765400?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Herlander</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+    </Container>
+  );
+}
