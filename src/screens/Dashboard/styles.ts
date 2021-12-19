507f0e0415388b70c84da9d3967c9e3edcdf2ad2
herlanderbento <herlanderbento19@gmail.com>
2021-12-19 14:46:53 +0100

    finalizando listagem de transacoes

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,7 +1,13 @@
 import styled from "styled-components/native";
+import { FlatList } from "react-native";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import { Feather } from "@expo/vector-icons";
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import {
+  getBottomSpace,
+  getStatusBarHeight,
+} from "react-native-iphone-x-helper";
+
+import { DataListProps } from ".";
 
 export const Container = styled.View`
   flex: 1;
@@ -58,8 +64,8 @@ export const UserName = styled.Text`
 `;
 
 export const Icon = styled(Feather)`
-  color: ${({ theme }) => theme.colors.secondary}
-  
+  color: ${({ theme }) => theme.colors.secondary};
+
   font-size: ${RFValue(24)}px;
 `;
 
@@ -71,14 +77,14 @@ export const HighlightCards = styled.ScrollView.attrs({
   width: 100%;
 
   position: absolute;
-  margin-top: ${RFPercentage(20)}px;
+  margin-top: ${RFPercentage(22)}px;
 `;
 
 export const Transactions = styled.View`
   flex: 1;
   padding: 0 24px;
 
-  margin-top: ${RFPercentage(12)}px;
+  margin-top: ${RFPercentage(14)}px;
 `;
 
 export const Title = styled.Text`
@@ -87,3 +93,12 @@ export const Title = styled.Text`
 
   margin-bottom: 16px;
 `;
+
+export const TransactionList = styled(
+  FlatList as new () => FlatList<DataListProps>
+).attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: getBottomSpace(),
+  },
+})``;
